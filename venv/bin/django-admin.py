4e069fe8bf4fc13d9721b32b718e1bcb84b689a9
Agaba-Milton <miltonagaba100@gmail.com>
2019-03-05 11:33:13 +0300

    hostels mgt system

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/emma/Desktop/AGABA/hote_system/Hostel-Management-System/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
